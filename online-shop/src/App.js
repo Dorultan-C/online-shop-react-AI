9e2ec560c5e5ce457474aa73e24952a055cbe8dd
Cristian Dorultan <c.dorultan@gmail.com>
2023-03-27 14:08:19 +0100

    clear code

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Online Shop</h1>
       </header>
+      <main>
+  <h2>Products</h2>
+  <ul>
+    <li>
+      <h3>Product 1</h3>
+      <p>Description of product 1</p>
+      <p>Price: $9.99</p>
+      <button>Add to Cart</button>
+    </li>
+    <li>
+      <h3>Product 2</h3>
+      <p>Description of product 2</p>
+      <p>Price: $19.99</p>
+      <button>Add to Cart</button>
+    </li>
+    <li>
+      <h3>Product 3</h3>
+      <p>Description of product 3</p>
+      <p>Price: $29.99</p>
+      <button>Add to Cart</button>
+    </li>
+  </ul>
+</main>
+
+      <footer>
+        <p>&copy; 2023 Online Shop</p>
+      </footer>
     </div>
   );
 }
