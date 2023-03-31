cc5354c4dd4944d50d3b815872a2cadaee89e286
Cristian Dorultan <c.dorultan@gmail.com>
2023-03-31 10:50:46 +0100

    second atempt to create from scruch

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import './App.css';
-
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Shop from './components/Shop';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Online Shop</h1>
-      </header>
-      <main>
-  <h2>Products</h2>
-  <ul>
-    <li>
-      <h3>Product 1</h3>
-      <p>Description of product 1</p>
-      <p>Price: $9.99</p>
-      <button>Add to Cart</button>
-    </li>
-    <li>
-      <h3>Product 2</h3>
-      <p>Description of product 2</p>
-      <p>Price: $19.99</p>
-      <button>Add to Cart</button>
-    </li>
-    <li>
-      <h3>Product 3</h3>
-      <p>Description of product 3</p>
-      <p>Price: $29.99</p>
-      <button>Add to Cart</button>
-    </li>
-  </ul>
-</main>
-
-      <footer>
-        <p>&copy; 2023 Online Shop</p>
-      </footer>
+      <Header />
+      {/* Use React Router to switch between Home, About, Shop, and Contact components */}
+      <Home />
+      <Footer />
     </div>
   );
 }
--- online-shop/src/index.js
+++ online-shop/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Shop from './components/Shop';
+import Contact from './components/Contact';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
